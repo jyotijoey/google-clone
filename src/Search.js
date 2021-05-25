f37e2afcd2e306b4f66fbb3034e13d21b16fc92a
jyotijoey <jburnwal5@gmail.com>
2021-05-25 13:51:53 +0530

    stateprovider and reducer functionality included in the app

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react'
-import "./Search.css"
-import SearchIcon from "@material-ui/icons/Search"
-import MicIcon from "@material-ui/icons/Mic"
-import { Button } from '@material-ui/core'
-import { useHistory } from 'react-router'
+import React, { useState } from 'react';
+import "./Search.css";
+import SearchIcon from "@material-ui/icons/Search";
+import MicIcon from "@material-ui/icons/Mic";
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import { useStateValue } from './StateProvider';
+import {actionTypes} from './reducer';
 
 function Search({hideButtons= false}) {
+    const [{}, dispatch] = useStateValue();
 
     const [input, setInput]= useState("");
-    const history = useHistory("");
+    const history = useHistory();
 
     const search= (e) => {
         e.preventDefault();
         console.log("you just hit the search btn", input);
 
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        });
+
         history.push("./search")
-    }
+    };
     return (
         <form className="search">
         <div className="search__input">
--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,9 @@
-import { Router } from "react-router";
 
 export const initialState={
     term:null,
 };
 
-export const actionType ={
+export const actionTypes ={
     SET_SEARCH_TERM: "SET_SERCH_TERM",
 };
 
@@ -12,7 +11,7 @@ const reducer =(state, action) => {
     console.log(action);
 
     switch (action.type) {
-        case actionType.SET_SEARCH_TERM:
+        case actionTypes.SET_SEARCH_TERM:
             return {
                 ...state,
                 term: action.term,
@@ -21,4 +20,4 @@ const reducer =(state, action) => {
     }
 };
 
-export default Router;
+export default reducer;
