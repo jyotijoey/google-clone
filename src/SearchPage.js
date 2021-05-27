77d7e5cbca02cdfb505d71ef4710454d36e04b45
jyotijoey <jburnwal5@gmail.com>
2021-05-27 06:57:36 +0530

    search functionality entered

---
+++ src/SearchPage.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import "./SearchPage.css"
+import { useStateValue } from './StateProvider'
+import useGoogleSearch from './useGoogleSearch';
+
+
+function SearchPage() {
+    const[{term}, dispatch] = useStateValue();
+    const {data} = useGoogleSearch(term);
+
+    console.log("the data is ====>>>>",data);
+    return (
+        <div className="searchPage">
+            <div className="searchPage__header">
+                <h1>{term}</h1>
+            </div>
+
+            <div className="searchPage__results">
+
+
+            </div>
+        </div>
+    )
+}
+
+export default SearchPage
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import keys from "./Keys";
+
+const id= keys.SEARCH_ENGINE_ID;
+const googleKey = keys.GOOGLE_KEY;
+
+
+const useGoogleSearch= (term) => {
+    const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+
+        const fetchData = async() => {
+            fetch(
+                `https://www.googleapis.com/customsearch/v1?key=${googleKey}&cx=${id}&q=${term}`
+            )
+            .then(response => response.json())
+            .then(result => {
+                setData(result);
+                
+            });
+        };
+        fetchData();
+        
+    }, [term]);
+
+    return {data};
+    
+};
+
+
+export default useGoogleSearch
